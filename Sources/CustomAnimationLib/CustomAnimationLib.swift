b2b059528df43a71c6e8b4d859de25719c4fe671
ajlani7105 <borent99@hotmail.com>
2025-06-08 13:16:30 +0300

    update AddKeyframeAction function in View extension

--- Sources/CustomAnimationLib/CustomAnimationLib.swift
+++ Sources/CustomAnimationLib/CustomAnimationLib.swift
@@ -11,10 +11,10 @@ extension Collection where Indices.Iterator.Element == Index {
  extension View {
     
     
-    public func AddKeyframeAction(action : [(() -> Void)],animation: Animation = .spring,duration: Double = 1,speed: Double = 0.5,delay: TimeInterval = 1
+    public func AddKeyframeAction(action : [(() -> Void)],delay: TimeInterval = 1
     ,Repeat: Bool = true) ->  some View {
         
-        self.modifier(AddCustomKeyframeAnimationModifier(frames:action,animation: animation,duration: duration,speed: speed,delay: delay,Repeat: Repeat))
+        self.modifier(AddCustomKeyframeAnimationModifier(frames:action,delay: delay,Repeat: Repeat))
 
         
     }
